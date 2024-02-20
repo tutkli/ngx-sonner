2a7216b7ad91014155973c181f5ca6481bff1975
Clara Castillo <claracastillochale@gmail.com>
2024-02-20 17:46:24 +0100

    fix: remove sonner provider from docs

--- apps/docs/src/app/app.config.ts
+++ apps/docs/src/app/app.config.ts
@@ -1,11 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideSonner } from 'ngx-sonner';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(appRoutes),
-    provideSonner({ toastLifetime: 1000000 }),
-  ],
+  providers: [provideRouter(appRoutes)],
 };
