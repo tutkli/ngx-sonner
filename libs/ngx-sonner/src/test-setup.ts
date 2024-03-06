c6337dea367c6f613c5057091e0749f31743ce41
Clara Castillo <claracastillochale@gmail.com>
2024-03-06 23:23:31 +0100

    ci: fix typo in test-setup

--- libs/ngx-sonner/src/test-setup.ts
+++ libs/ngx-sonner/src/test-setup.ts
@@ -1,6 +1,6 @@
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-enjestronment
+// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
 globalThis.ngJest = {
-  testEnjestronmentOptions: {
+  testEnvironmentOptions: {
     errorOnUnknownElements: true,
     errorOnUnknownProperties: true,
   },
