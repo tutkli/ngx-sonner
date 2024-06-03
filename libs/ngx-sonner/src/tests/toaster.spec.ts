59c1aca734f399a4a4e0ad60fcccda7666a7b068
Clara Castillo <claracastillochale@gmail.com>
2024-06-03 10:43:07 +0200

    fix: allow to render custom icons for toasts
    fixes #10

--- libs/ngx-sonner/src/tests/toaster.spec.ts
+++ libs/ngx-sonner/src/tests/toaster.spec.ts
@@ -284,4 +284,16 @@ describe('Toaster', () => {
     const closeButton = container.querySelector('[data-close-button]');
     expect(closeButton).not.toBeNull();
   });
+
+  it('should render the custom icon when provided', async () => {
+    const { user, trigger, container } = await setup({
+      callback: toast => toast.success('Hello world'),
+    });
+    await user.click(trigger);
+    const icon = container.querySelector('[data-icon]');
+    expect(icon).not.toBeNull();
+    expect(icon).toContainHTML(
+      '<svg success-icon xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-check"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" /><path d="m9 12 2 2 4-4" /></svg>'
+    );
+  });
 });
